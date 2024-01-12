6f5a6348502e7a00b1ffd834d8cdc8f47a40fbf5
kinglyu <kinglshadow@126.com>
2024-01-12 11:54:46 +0800

    103

--- update.sh
+++ update.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-rm ~/Desktop/Project/code/mycydia/Packages
-rm ~/Desktop/Project/code/mycydia/Packages.bz2
-rm ~/Desktop/Project/code/mycydia/Packages.gz
-#rm Packages Packages.bz2 Packages.gz
+cd ~/Desktop/Project/code/mycydia
+rm Packages Packages.bz2 Packages.gz
 
-dpkg-scanpackages -m ~/Desktop/Project/code/mycydia/. > ~/Desktop/Project/code/mycydia/Packages
-bzip2 -k ~/Desktop/Project/code/mycydia/Packages
-gzip -c ~/Desktop/Project/code/mycydia/Packages > ~/Desktop/Project/code/mycydia/Packages.gz
+dpkg-scanpackages -m . > Packages
+bzip2 -k Packages
+gzip -c Packages > Packages.gz
